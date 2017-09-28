f9fbf98772fa65ae677273eb2efd810004650a50
Kota Nonaka <kota.kaicho@gmail.com>
2017-09-29 00:18:17 +0900

    団体の追加をモーダルに変更

--- src/renderer/screens/AddProject/view.js
+++ src/renderer/screens/AddProject/view.js
@@ -4,15 +4,75 @@ import {
   Table,
   Button,
   Icon,
-  Tab
+  Tab,
+  Input,
+  Modal,
+  Form,
+  Dropdown
 } from 'semantic-ui-react'
 import store from '../../store'
 import { push } from 'react-router-redux'
 import ReturnMenuButton from '../../component/returnMenu'
 
 const AddProject = (groupName) => {
+  const sectionOptions = [
+    {
+      text: '中学発表',
+      value: 'junior-high-school'
+    },
+    {
+      text: '高校発表',
+      value: 'high-school-presentation'
+    },
+    {
+      text: '高校販売',
+      value: 'high-school-seling'
+    },
+    {
+      text: '部活・同好会・有志',
+      value: 'club'
+    },
+    {
+      text: '後夜祭',
+      value: 'after-party'
+    }
+  ]
+
   return (
     <div style={{padding: '10px'}}>
+      <Modal
+        trigger={<Button>団体追加</Button>}
+      >
+        <Modal.Header>団体追加</Modal.Header>
+        <Modal.Content>
+          <Form>
+            <Form.Field>
+              <label>No</label>
+              <p>1</p>
+            </Form.Field>
+            <Form.Field>
+              <label>区分</label>
+              <Dropdown
+                placeholder='区分を選択...'
+                options={sectionOptions}
+                fluid
+                section
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>団体名</label>
+              <Input fluid/>
+            </Form.Field>
+            <Form.Field>
+              <label>企画名</label>
+              <Input fluid/>
+            </Form.Field>
+          </Form>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button primary>追加</Button>
+        </Modal.Actions>
+      </Modal>
       <Table celled>
         <Table.Header>
           <Table.Row>
@@ -24,8 +84,8 @@ const AddProject = (groupName) => {
         <Table.Body>
           <Table.Row>
             <Table.Cell>1</Table.Cell>
-            <Table.Cell>{groupName}</Table.Cell>
-            <Table.Cell>ほげふがぴよ</Table.Cell>
+            <Table.Cell><Input fluid/></Table.Cell>
+            <Table.Cell><Input fluid/></Table.Cell>
           </Table.Row>
         </Table.Body>
       </Table>
@@ -45,10 +105,13 @@ const AddProjectTab = () => {
   return (
     <div>
       <Header as='h2' content='企画登録'/>
-      <Tab panes={panes} />
+      <Tab panes={panes}/>
       <div style={{textAlign: 'right'}}>
-        <Button style={{marginTop: '0.25em', marginRight: '10px'}} color='green' onClick={() => store.dispatch(push('/top'))}><Icon name='save'/>保存</Button>
-        <ReturnMenuButton />
+        <Button
+          style={{marginTop: '0.25em', marginRight: '10px'}} color='green'
+          onClick={() => store.dispatch(push('/top'))}
+        ><Icon name='save'/>保存</Button>
+        <ReturnMenuButton/>
       </div>
     </div>
   )
